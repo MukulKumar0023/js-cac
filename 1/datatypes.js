e88fdcf2178934624410bda129f4e00d64a5537c
MukulKumar0023 <130737647+MukulKumar0023@users.noreply.github.com>
2024-05-17 18:40:00 +0000

    Add Datatypes.js

---
+++ 1/datatypes.js
@@ -0,0 +1,27 @@
+"use strict"; // Treat all code based on newer versionof js.
+
+// alert("HI"); // we are using node.js,not browser.
+
+console.log(
+  3 + // Code Readability should be high.
+    3
+);
+
+let name = "Mukul";
+let age = 24;
+let isLoggedIn = true;
+let temp = null;
+let state;
+
+console.log(typeof(temp)); // null is an object
+console.log(typeof temp);
+
+// Primitive DataTypes.
+// number => 2e53;
+// bigint
+// boolean => true/false;
+// string => "";
+// null => standalone value; (Null is the representation of an empty value,null does'nt mean "" or 0).
+// undefined =>
+// symbol => uniqueness.
+
---
+++ 1/variables.js
@@ -0,0 +1,10 @@
+const accountId = 11111;
+console.log(accountId);
+var accountEmail = "mukulverygood@gmail.com";
+console.log(accountEmail);
+{
+    let accountPassword = "12345678";
+    console.log(accountPassword);
+}
+accountCity = "Ghaziabad";
+console.log(accountCity);
