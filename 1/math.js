49320312b9103c634a06153a5e084941823285ed
MukulKumar0023 <130737647+MukulKumar0023@users.noreply.github.com>
2024-05-29 18:11:13 +0000

    Add Math.js File

---
+++ 1/math.js
@@ -0,0 +1,20 @@
+// console.log(Math);
+
+// console.log(Math.floor(Math.random() * 10)); 
+// This gives me value in range (0 <= N < 10).
+
+// console.log(Math.floor(Math.random() * 10) + 1); 
+// This gives me value in range (1 <= N <= 10).
+
+// console.log(Math.floor(0.01 * 10));
+// console.log(Math.floor(0.99 * 10) + 1);
+
+// Setting up static range.
+const min = 100;
+const max = 200;
+
+// console.log(Math.floor(Math.random() * (max - min) + min));
+// This gives me value in range (100 <= N <= 199).
+
+// console.log(Math.floor(Math.random() * (max - min + 1) + min));
+// This gives me value in range (100 <= N <= 200).
